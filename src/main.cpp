df5056b4321e158443bcb6f9b6c1021bea04b989
wanghj <sohumango@yahoo.com>
2021-03-22 00:56:17 +0900

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,106 @@
+#include <Arduino.h>
+#include <Servo.h>
+
+Servo svAnsw;  // create servo object to control a servo
+Servo svOpen;  // create servo object to control a servo
+
+const int D0 = 10, D1 = 11, D2 = 12, D3 = 13; // Variables correspondientes a las entradas del receptor
+const int nmaxOpen = 65;
+const int nmaxAnsw = 70;
+
+void pushButton(Servo ser, int nmax){
+    for (int pos = 0; pos <= nmax; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    ser.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  delay(200);  
+  for (int pos2 = nmax; pos2 >= 0; pos2 -= 1) { // goes from 180 degrees to 0 degrees
+    ser.write(pos2);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+}
+
+int arr_screenoff[10];
+int nignore = 0;
+bool bdebug = true;
+
+bool judge_lights(){
+  nignore++;
+  if(nignore<300){
+    delay(10);
+    return false;
+  }
+  nignore=0;
+  int len_off = sizeof(arr_screenoff)/sizeof(int);
+  int sum = 0;
+  for(int i=0; i<len_off; i++){
+    sum += arr_screenoff[i];
+  }
+  int avg = sum/len_off;
+  int currVal = analogRead(A0);
+  if(currVal-avg<6){
+    for(int i=0; i<len_off-1; i++){
+      arr_screenoff[i] = arr_screenoff[i+1];
+    }
+    arr_screenoff[len_off-1] = currVal;
+    Serial.print(currVal);
+    Serial.println(" is put to last position");
+  } else if(currVal-avg>10) {
+    return true; // only screen off value put to array.
+  }
+  return false;
+}
+
+void setup() {
+  svAnsw.attach(5);
+  svOpen.attach(6);
+  pinMode(D0, INPUT); // Entrada digital correspondiente al boton B
+  pinMode(D1, INPUT); // Entrada digital correspondiente al boton D
+  pinMode(D2, INPUT); // Entrada digital correspondiente al boton A
+  pinMode(D3, INPUT); // Entrada digital correspondiente al boton C  
+  Serial.begin(9600); // Velocidad de comunicacion de 9600 bps
+  //init screen off value with current value.
+  int len_off = sizeof(arr_screenoff)/sizeof(int);
+  int startValue = analogRead(A0);
+  Serial.print("init sreen off array with current: ");
+  Serial.println(startValue);
+  for(int i = 0; i <len_off; i++){
+    arr_screenoff[i] = startValue;;
+  }
+}
+
+void loop() {
+    svOpen.write(0);
+    svAnsw.write(0);
+  if (digitalRead(D3) == HIGH){ // Button 1
+    Serial.println("Boton: C");
+    pushButton(svAnsw, nmaxAnsw); //answer screen
+    delay(500);
+    pushButton(svOpen, nmaxOpen); // unlock
+    delay(500);
+    pushButton(svAnsw, nmaxAnsw); // close screen
+  }
+
+  if (digitalRead(D2) == HIGH){ // Button 2
+    Serial.println("Button: A");
+    pushButton(svAnsw, nmaxAnsw);
+    delay(500);
+  }
+  if (digitalRead(D1) == HIGH){ // Button 3
+    Serial.println("Botton: D");
+    pushButton(svOpen, nmaxOpen);;
+    delay(500);
+  }
+
+
+  if (digitalRead(D0) == HIGH){Serial.println("Boton: B"); delay(500);}
+  if(judge_lights()==true){
+    Serial.print("Screen on\n");
+  }
+  if (Serial.available() > 0) {
+    char x = Serial.read();
+    Serial.print("Read byte:");
+    Serial.println(x);
+  }
+}
